c5f738c5d3704fa911eaa7c5e46256e14bf48f6e
Andrew Miracle <me@andrewmiracle.com>
2023-08-14 20:15:20 +0000

    implement the trpc panel

---
+++ src/pages/api/panel.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { renderTrpcPanel } from "trpc-panel";
+import { appRouter } from "@/server/api/root";
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  res.status(200).send(
+    renderTrpcPanel(appRouter, {
+      url: "http://localhost:3000/api/trpc",
+      transformer: "superjson",
+    })
+  );
+}
