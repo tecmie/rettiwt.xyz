4b429a277b0d1408ba9e8a05ced586501772c60f
Andrew Miracle <me@andrewmiracle.com>
2023-08-24 09:14:30 +0000

    update env validator

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE___PRISMA_URL: z.string().url(),
     NODE_ENV: z.enum(['development', 'test', 'production']),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === 'production'
@@ -44,7 +44,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE___PRISMA_URL: process.env.DATABASE___PRISMA_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
